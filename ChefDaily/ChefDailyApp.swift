219f0b06f630eb3e743192727889b01665730630
ruben140 <ruben140@icloud.com>
2024-09-16 21:30:19 +0200

    Initial empty xcode project

---
+++ ChefDaily/ChefDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDailyApp.swift
+//  ChefDaily
+//
+//  Created by Ruben de Koning on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChefDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
